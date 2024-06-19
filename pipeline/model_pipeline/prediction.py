2a08ad213cf18554d0c7b55916f40821ccf6ebe3
Saransh Bhandari <saranshbhandari28@gmail.com>
2024-06-19 23:35:30 +0530

    Add inference, trigger code

---
+++ pipeline/model_pipeline/prediction.py
@@ -0,0 +1,20 @@
+from actions.open_gate import trigger_door_alert
+threshold = 0.6
+
+def load_model(path):
+    # model = joblib.load(...)
+    pass
+
+def make_prediction(model, data):
+    # prediction = model.predict(data)
+    pass
+
+def criteria(prediction):
+    return prediction > threshold
+
+def inference(data):
+    model = load_model('path/to/model')
+    prediction = make_prediction(model, data)
+    if criteria(prediction):
+        trigger_door_alert({'MESSAGE':'OPEN GATE !!!'})
+
